7a47f773db30384531f20e2041cec1678f12c547
Chris <csfrashure@gmail.com>
2020-08-08 21:10:09 -0400

    added sessions, mysql session store

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,9 +1,18 @@
 
 
 const controller = {
+    
+    // createUser: (req, res) => {
+    //     const pw = req.body.password;
+    //     const encryptedPW;
+
+
+    // },
+
     getUser: (req, res) => {
         res.send("getUser route suceeded!");
     },
+
     postUser: (req, res) => {
         res.send("postUser route succeeded!");
     }
--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mysqlStore = require('express-mysql-session')(session);
 require('dotenv').config();
 
 const port = process.env.port || 3232;
 
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json);
+
+app.use(session({
+    secret: 'alksjdvkla2432uoiuyaw4ouhtjlzh52',
+    resave: true,
+    saveUninitialized: true,
+    store: new mysqlStore({
+        host: process.env.dbHost,
+        port: process.env.dbPort,
+        user: process.env.dbUser,
+        password: process.env.dbPass,
+        database: process.env.dbSchema
+    })
+}))
+
+const usersRoutes = require('./routes/userRoutes');
+app.use('/users', usersRoutes);
+
 app.listen(port, () => {
     console.log(`Verdict app listening on port ${port}`);
 });
-
-//
-const usersRoutes = require('./routes/userRoutes');
-app.use('/users', usersRoutes);
